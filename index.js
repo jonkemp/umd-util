a99f16d9969e52b67a425086121a3746e683f06d
Jonathan Kemp <kempdogg@gmail.com>
2020-01-28 08:14:25 -0500

    Swtich umd modules to support CommonJS format.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const makeDir = require('make-dir');
-const umdify = require('libumd');
+const umdify = require('umd');
 
-module.exports = (fileDir, filePath, options) => {
+module.exports = (moduleName, fileDir, filePath, commonJS) => {
 	try {
 		const dir = fileDir || 'umd';
-		const getPath = _filePath => path.join(dir, _filePath);
-		const umdFilePath = getPath(filePath);
+		const umdFilePath = path.join(dir, filePath);
 		const fileContent = fs.readFileSync(filePath, 'utf8');
 		makeDir.sync(path.dirname(umdFilePath));
-		fs.writeFileSync(umdFilePath, umdify(fileContent, options));
+		fs.writeFileSync(umdFilePath, umdify(moduleName, fileContent, commonJS));
 	} catch (error) {
 		throw new Error(error);
 	}
