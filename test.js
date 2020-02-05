50e383f7ce57ff574f2ca593bc4f5c31b0dbe2c8
Jonathan Kemp <kempdogg@gmail.com>
2020-02-04 22:08:59 -0500

    Update tests.

--- test.js
+++ test.js
@@ -6,12 +6,14 @@ const beautify = require('js-beautify').js;
 const umdify = require('./index.js');
 
 describe('umdify', () => {
-    it('should return a umd compatible script from a module with browser globals', () => {
+    it('should return a umd compatible script from a module with browser globals', async () => {
         const beautifyOptions = {
 			indent_with_tabs: true
 		};
 
-        umdify('test', tempDirectory, path.join('test', 'browser.js'));
+        await umdify('test', path.join('test', 'browser.js'), {
+			destination: tempDirectory
+		});
 
 		const code = fs.readFileSync(path.join(tempDirectory, 'test', 'browser.js'), 'utf-8');
 		const compare = fs.readFileSync(path.join(__dirname, 'test', 'fixture', 'browser.js'), 'utf-8');
@@ -19,12 +21,15 @@ describe('umdify', () => {
 		assert.equal(beautify(code, beautifyOptions), beautify(compare, beautifyOptions));
 	});
 
-	it('should return a umd compatible script from a CommonJS module', () => {
+	it('should return a umd compatible script from a CommonJS module', async () => {
         const beautifyOptions = {
 			indent_with_tabs: true
 		};
 
-        umdify('myFunc', tempDirectory, path.join('test', 'cjs.js'), true);
+        await umdify('myFunc', path.join('test', 'cjs.js'), {
+			commonJS: true,
+			destination: tempDirectory
+		});
 
 		const code = fs.readFileSync(path.join(tempDirectory, 'test', 'cjs.js'), 'utf-8');
 		const compare = fs.readFileSync(path.join(__dirname, 'test', 'fixture', 'cjs.js'), 'utf-8');
